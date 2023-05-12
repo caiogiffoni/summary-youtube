c452ccb5a1ebb70d3cb6a0f232d01e21e3575a46
Caio Giffoni <caio_cgf@hotmail.com>
2023-05-11 22:28:10 -0300

    started returning summary

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import os
-import sys
-import re
-from youtube_transcript_api import YouTubeTranscriptApi
-import openai
-from dotenv import load_dotenv
 import math
+import os
+import re
+import sys
 
 import ipdb
-
+import openai
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
 
 load_dotenv()
 
@@ -20,39 +19,53 @@ def get_transcript(link):
         [video_id], languages=["pt", "en"]
     )
     # transcript_list = [t['text'] for t in transcript[0][video_id]]
-    blocks = math.ceil(
-        transcript_response[0][video_id][-1]["start"] / (60 * 5)
-    )
+    seconds = 60 * 5
+    blocks = math.ceil(transcript_response[0][video_id][-1]["start"] / seconds)
+    transcript_list = []
     for i in range(blocks):
-        print((i + 1) * 300)
-        banan.append(
+        transcript_list.append(
             [
                 t["text"]
-                for t in response[0][video_id]
-                if t["start"] > (i) * 300 and t["start"] <= (i + 1) * 300
+                for t in transcript_response[0][video_id]
+                if t["start"] > (i) * seconds
+                and t["start"] <= (i + 1) * seconds
             ]
         )
-    transcript_list = [t["text"] for t in transcript_response[0][video_id]]
-    ipdb.set_trace()
-    return " ".join(transcript_list)
+    return [" ".join(t) for t in transcript_list]
 
 
-def get_summary(transcript):
+def get_summary(transcript: list):
     openai.api_key = os.getenv("OPENAI_API")
-    # response = openai.Completion.create(
-    #     engine='text-davinci-003',
-    #     prompt=f'Esse texto é uma transcrição de uma video do youtube. Pode resumir, mas mantendo todos pontos principais discutidos? {transcript}',
-    #     temperature=0.9,
-    #     max_tokens=2048,
-    #     n=1,
-    #     stop=None
-    # )
+    # transcript_list = [t["text"] for t in transcript_response[0][video_id]]
+    response = []
     # ipdb.set_trace()
-    # return response['text']
-    return "oi"
+    for part in transcript:
+        # ipdb.set_trace()
+        prompt = f"Resuma essa transcição de vídeo do Youtube: '{part}'"
+        response.append(
+            openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=2048,
+                n=1,
+                stop=None,
+                temperature=0.8,
+            )
+        )
+    treated_transcript = [t["choices"][0]["text"] for t in response]
+    return " ".join(treated_transcript)
 
 
 link = sys.argv[1]
 transcript = get_transcript(link)
 summary = get_summary(transcript)
 print(summary)
+
+# prompt = f"Quero que resuma essa transcição de vídeo do Youtube: {part}"
+# promptx = "quanto é 5x5"
+# banana = openai.Completion.create(
+#     model="text-davinci-003",
+#     prompt=promptx,
+#     temperature=0.9,
+#     max_tokens=2048,
+# )
