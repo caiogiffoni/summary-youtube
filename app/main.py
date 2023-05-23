e4cc8f9e6a5d1696abbcee20a602eca4604f6e57
Caio Giffoni <caio_cgf@hotmail.com>
2023-05-22 21:44:27 -0300

    added cors permissions

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import sys
 import openai
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from youtube_transcript_api import YouTubeTranscriptApi
 
 from app.models import SummarizeText
@@ -18,6 +19,14 @@ load_dotenv()
 app = FastAPI()
 
 logger = logging.getLogger(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/summarize")
@@ -78,7 +87,9 @@ async def summarize(body: SummarizeText):
     logger.info("Link Received")
     transcript = get_transcript(body.link)
     logger.info(f"Transcript obtained for requested video")
-    summary = get_summary(transcript)
+    # summary = get_summary(transcript)
     logger.info("Summary Generated!")
 
-    return {summary}
+    return [
+	"\n\nA crise econômica iniciada no início do ano tem feito vítimas, como as Lojas Americanas, que acabaram indo à falência. Outras empresas do mesmo segmento, como o Magalu, Via Varejo e Lojas Marisa, também tiveram prejuízos colossais. O Magalu, por exemplo, apresentou o maior prejuízo para um primeiro trimestre desde que foi para a Bolsa em 2011. Por sua vez, o Via Varejo teve um prejuízo de 297 milhões, somando juros pagos e recebidos. Já as Lojas Marisa tiveram um prejuízo de 149 milhões de reais e 10 lojas físicas alvos de despejo por falta de pagamento de aluguel. O e-commerce brasileiro também teve redução de quase 40%, mas as vendas do e-commerce da Magalu avançaram 11%. Parece que a queda nas vendas das Lojas Americanas foi aproveitada por outras empresas, como o Magalu. \n\nA maior parte dos produtos pode ser encontrada no Mercado Livre e nas lojas chinesas, dependendo do que está sendo comprado. O grande motivo do péssimo resultado da Magalu é o alto endividamento devido ao pagamento de juros. A dívida total da Magalu é de 7 bilhões e 271 milhões, enquanto que a dívida total da Lojas Marisa é de 737 milhões. A capacidade de pagamento da Magalu pode ser medida pela relação da receita financeira em relação ao resultado operacional, que é de 2,38, enquanto que o resultado operacional da Marisa foi negativo nos últimos 12 meses, mostrando prejuízo de 149 milhões. Devido ao alto endividamento, três empresas entraram com um pedido de falência na justiça. \n\nNeste vídeo, foi discutida a situação atual do varejo brasileiro. Se você tiver algum comentário para compartilhar, por favor deixe-o nos comentários abaixo. O nosso bate-papo chegou ao fim, mas até a próxima!"
+]
